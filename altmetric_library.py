3e823ccf5ee4c47c4dea62b223f2d1b63677e595
Lauren Revere <reverel17@mail.wlu.edu>
2014-07-07 11:39:16 -0400

    "Cleaned up line length. Fixed articles_from_timeframe."

--- altmetric_library.py
+++ altmetric_library.py
@@ -1,5 +1,7 @@
 """
-This is a python library for the altmetrics API.
+This is a python library for the Altmetric API.
+
+For more information on the Altmetric API visit http://api.altmetric.com/.
 
 Some pieces of this library were inspired by or derived from the altmetric api
 wrapper altmetric.py which is licensed under the MIT open source license.
@@ -18,7 +20,8 @@ class AltmetricException(Exception):
 
 class JSONParseException(AltmetricException):
     """
-    Failed to turn HTTP Response into JSON. Site is probably in the wrong format.
+    Failed to turn HTTP Response into JSON.
+    Site is probably in the wrong format.
     """
     pass
 
@@ -34,10 +37,9 @@ class AltmetricHTTPException(AltmetricException):
             response_codes.get(status_code, status_code)
         )
 
-class Altmetric():
+class Altmetric(object):
     def __init__(self, api_key = None, api_version = 'v1'):
         """Cache API key and version."""
-        self._api_key = api_key
         self._api_version = api_version
         if self._api_version != 'v1':
             warnings.warn("This library has only been tested with API v1.\
@@ -46,7 +48,7 @@ class Altmetric():
         self._api_url = "http://api.altmetric.com/%s/" % self.api_version
 
         self._api_key = {}
-        if self._api_key:
+        if api_key:
             self._api_key = {'key': api_key}
 
     #Make articles
@@ -76,27 +78,27 @@ class Altmetric():
         raw_json = self._get_altmetrics('arxiv', arxiv_id)
         return self._create_article(raw_json)
 
-    def articles_from_timeframe(self, timeframe, page = 1,
-        num_results = 100, **kwargs):
+    def articles_from_timeframe(self, timeframe, page = 1, num_results = 100,
+        doi_prefix = None, nlmid = None, subjects = None, cited_in = None):
 
-        #accepts both 1d and 1 day eventually or should i not care?
-        #is it okay to have **kwargs when i already have some?
-        #should these be explicit? cited_in doi_prefix
-        #nlmid subject
-
-        i = page
+        #timeframe = self._check_timeframe(timeframe)
+        #should i collect metadata about the query? where do i store that?
 
         while(1):
-            raw_json = self._get_altmetrics('citations', timeframe, page = i, kwargs)
+            raw_json = self._get_altmetrics('citations', timeframe,
+                page = page, num_results = num_results ,
+                doi_prefix = doi_prefix, nlmid = nlmid,
+                subjects = subjects, cited_in = cited_in)
+            page += 1
             if not raw_json:
                 break
-            results = raw_json.get('results', {})
-            i += 1
-            for result in results:
-                yield Article(result)
-            #yield [Article(x) for x in results if x]
+            for result in raw_json.get('results', []):
+                yield self._create_article(result)
 
     def _get_altmetrics(self, method, *args, **kwargs):
+        """
+        Request information from Altmetric. Return a dictionary.
+        """
         request_url = self.api_url + method + "/" + "/".join([a for a in args])
         params = kwargs or {}
         params.update(self.api_key)
@@ -106,8 +108,8 @@ class Altmetric():
                 return response.json()
             except ValueError as e:
                 raise JSONParseException(e.message)
-        elif response.status_code == 404 or 400:
-            return None
+        elif response.status_code in (404, 400): #should i handle this differently
+            return {}
         else:
             raise AltmetricHTTPException(response.status_code)
 
@@ -131,8 +133,6 @@ class Altmetric():
 
 
 class Article():
-
-    
     def __init__(self, raw_json):
         """
         Create an article object. Get raw dictionary from
@@ -149,11 +149,12 @@ class Article():
         #Article Info
         self._title = self._raw.get('title')
         self._abstract = self._raw.get('abstract')
-        self._abstract_scource = self._raw.get('abstract_source')
+        self._abstract_source = self._raw.get('abstract_source')
         self._journal = self._raw.get('journal')
         self._subjects = self._raw.get('subjects', [])
         self._added_on = self._convert_to_utc(self._raw.get('added_on'))
-        self._published_on = self._convert_to_utc(self._raw.get('published_on'))
+        self._published_on = self._convert_to_utc(
+            self._raw.get('published_on'))
         self._url = self._raw.get('url')
         self._is_open_access = self._raw.get('is_oa')
 
@@ -174,30 +175,39 @@ class Article():
         
         #Altmetrics
         self._score = self._raw.get('score')
-        self._score_history = self._parse_score_history(self._raw.get('history', {}))
-        self._score_context = self._parse_score_context(self._raw.get('context', {}))  
-        self._last_updated = self._convert_to_utc(self._raw.get('last_updated'))
+        self._score_history = self._parse_score_history(
+            self._raw.get('history', {}))
+        self._score_context = self._parse_score_context(
+            self._raw.get('context', {}))  
+        self._last_updated = self._convert_to_utc(
+            self._raw.get('last_updated'))
         self._downloads = self._raw.get('downloads', 0)
-        
         self._schema  = self._raw.get('schema') #FIX schema for what
         
-        self._cited_by_facebook_walls_count = self._raw.get('cited_by_fbwalls_count', 0)
+        self._cited_by_facebook_walls_count = self._raw.get(
+            'cited_by_fbwalls_count', 0)
         self._cited_by_redits_count = self._raw.get('cited_by_rdts_count', 0)
-        self._cited_by_tweeters_count = self._raw.get('cited_by_tweeters_count', 0)
-        self._cited_by_google_plus_count = self._raw.get('cited_by_gplus_count', 0)
+        self._cited_by_tweeters_count = self._raw.get(
+            'cited_by_tweeters_count', 0)
+        self._cited_by_google_plus_count = self._raw.get(
+            'cited_by_gplus_count', 0)
         self._cited_by_msm_count = ('cited_by_msm_count', 0)
         self._cited_by_delicious_count = ('cited_by_delicious _count', 0)
         self._cited_by_qs_count = self._raw.get('cited_by_qs_count', 0)
         self._cited_by_posts_count = self._raw.get('cited_by_posts_count', 0)
-        self._cited_by_accounts_count = self._raw.get('cited_by_accounts_count', 0) or \
-        self._raw.get('by_accounts_count', 0)
+        self._cited_by_accounts_count = (
+            self._raw.get('cited_by_accounts_count', 0)
+            or self._raw.get('by_accounts_count', 0)
+        )
+
         self._cited_by_forums_count = ('cited_by_forum_count', 0)
-        self._peer_review_sites_count = ('cited_by_peer_review_sites_count', 0)
+        self._cited_by_peer_review_sites_count = \
+        ('cited_by_peer_review_sites_count', 0)
         self._cited_by_feeds_count = ('cited_by_feeds_count', 0)
         self._cited_by_videos_count = ('cited_by_videos_count', 0)
 
 
-        self._cohorts = self._raw.get('cohorts', {}) #FIX what is this
+        self._cohorts = self._raw.get('cohorts', {})
         
         self._readers_count = ('readers_count', 0)
         self._readers = self._raw.get('readers', {})
@@ -227,10 +237,14 @@ class Article():
     def _convert_to_utc(self,unix_time):
         """Convert UNIX timestamp to UTC."""
         if isinstance(unix_time, float): #this line could cause us to lose data
-            return datetime.datetime.fromtimestamp(unix_time).strftime('%Y-%m-%dT%H:%M:%Sz')
+            return datetime.datetime.fromtimestamp(unix_time).strftime(
+                '%Y-%m-%dT%H:%M:%Sz')
 
     def _parse_publisher_subjects(self, subjects):
-        """Turns the publisher_subjects list of dictionaries into a list of subjects."""
+        """
+        Turns the publisher_subjects list of dictionaries into a list of 
+        subjects.
+        """
         new_subjects = []
         if subjects:
             for item in subjects:
@@ -238,13 +252,16 @@ class Article():
         return new_subjects
 
     def _parse_score_context(self, context): #FIX possibally change the word percent
-        """Change the names of the dictionaries in context to make more sense."""
+        """
+        Change the names of the dictionaries in context to make more sense.
+        """
         new_context = {}
         if context:
-            new_context['journal age'] = context.get('similar_age_journal_3m', {})
+            new_context['journal age'] = context.get(
+                'similar_age_journal_3m', {})
             new_context['context age'] = context.get('similar_age_3m', {})
             new_context['journal'] = context.get('journal', {})
-        return new_context
+        return new_context #key atribute error could happen here. how to deal?
 
     def __repr__(self): #FIX unicode problems
         return self.title[:12].encode('UTF-8')
@@ -369,9 +386,10 @@ class Article():
     @property
     def score_context(self):
         """
-        Return a dictionary that allows you to compare an article's popularity to
-        articles of a 'similar age'(published within 6 weeks on either side), articles
-        in journals of a 'similar age', and other articles in the same 'journal'.
+        Return a dictionary that allows you to compare an article's popularity
+        to articles of a 'similar age'(published within 6 weeks on either
+        side), articles in journals of a 'similar age', and other articles in
+        the same 'journal'.
         """
         return self._score_context
     
@@ -384,7 +402,7 @@ class Article():
     
     @property
     def cited_by_redits_count(self):
-        return self._redit_mentions
+        return self._cited_by_redits_count
     @property
     def cited_by_tweeters_count(self):
         return self.cited_by_tweeters_count
@@ -423,8 +441,8 @@ class Article():
         return self._cited_by_feeds_count
 
     @property
-    def cited_by_peer_review_count(self):
-        return self._cited_by_peer_review_count
+    def cited_by_peer_review_sites_count(self):
+        return self._cited_by_peer_review_sites_count
     
     @property
     def cited_by_accounts_count(self):
@@ -442,18 +460,20 @@ class Article():
     @property
     def readers(self):
         """
-        Return a  dictionary that contains information about the numbers of readers on
-        various reference manager websites. The website name is the key and the number of
-        readers is the value. Ex. {'mendeley': 11, , 'citeulike': 0, 'connotea' : 4}
+        Return a  dictionary that contains information about the numbers of
+        readers on various reference manager websites. The website name is the
+        key and the number of readers is the value.
+        Ex. {'mendeley': 11, , 'citeulike': 0, 'connotea' : 4}
         """
         return self._readers
 
     @property
     def cohorts(self): #FIX maybe change so more clear
         """
-        Return a dictionary with the number of people mentioning this article who are
-        members of the public (pub), practitioners (doc), research scientists (sci) or
-        science communicators (com) (This is an experimental Altmetric feature).
+        Return a dictionary with the number of people mentioning this article
+        who are members of the public (pub), practitioners (doc), research
+        scientists (sci) or science communicators (com)
+        (This is an experimental Altmetric feature).
         """
         return self._cohorts
     
@@ -465,7 +485,8 @@ class Article():
     @property
     def altmetric_images(self):
         """
-        Return a dictionary of the altmetric score image in 'small', 'medium', and 'large'
+        Return a dictionary of the altmetric score image in
+        'small', 'medium', and 'large'.
         """
         return self._altmetric_images
 
@@ -478,6 +499,7 @@ if __name__ == "__main__": #for mini tests
 
     metric_object = Altmetric(api_key)
     articles = metric_object.articles_from_timeframe("1d")
+    #article = metric_object.article_from_doi("10.1038/news.2011.49")
     for list_ in articles:
-     print list_
+        print list_
 
